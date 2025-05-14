9d3993993570a9a3befd524450c381d24bb92654
Quixelot <averagep5rakechifan@gmail.com>
2025-05-14 17:30:28 -0400

    Hoy trabaje mas en el estilo del titulo y le anadi un temporizador

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,57 @@ import { RxStopwatch } from "react-icons/rx";
 import { LuClock9 } from "react-icons/lu";
 import Stack from 'react-bootstrap/Stack';
 import Button from 'react-bootstrap/Button';
+import { CountdownCircleTimer } from 'react-countdown-circle-timer'
+import styled from 'styled-components';
 
 
 
+const Title = styled.h1`
+  font-size: 2.5em;
+  text-align: center;
+  color: black;
+`;
 
+const Wrapper = styled.section`
+  padding: 4em;
+  background:rgb(144, 212, 255);
+`;
 
 
 function App (){
 
     
     return (
-
+                
         
         <div className="Container">
+
+   <Wrapper>
+    <Title>ONLINE TIMER</Title>
+   </Wrapper>
+
+   <div className="Body">     
    <Stack gap={1}>
-      <div className="p-2"><button><GiAlarmClock></GiAlarmClock></button></div>
+      <div className="p-2"><button onCl><GiAlarmClock></GiAlarmClock></button></div>
       <div className="p-2"><button><RxTimer></RxTimer></button></div>
       <div className="p-2"><button><RxStopwatch></RxStopwatch></button></div>
       <div className="p-2"><button><LuClock9></LuClock9></button></div>
     </Stack>
-        </div>
+        
+
+         <CountdownCircleTimer
+    isPlaying
+    duration={9}
+    colors={['#004777', '#F7B801', '#A30000', '#A30000']}
+    colorsTime={[7, 5, 2, 0]}
+  >
+    {({ remainingTime }) => remainingTime}
+  </CountdownCircleTimer>
+  </div>
+
+  </div>
+
+
     )
 }
 
