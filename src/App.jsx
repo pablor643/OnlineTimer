fe57f57f3fcbae13e868d29acf5d262e6367f127
Quixelot <averagep5rakechifan@gmail.com>
2025-05-19 17:30:08 -0400

    Hoy estuve trabajando en la funcion del boton para play and pause, ademas que el input del usuario esa el tiempo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,6 @@
-import Image from 'react-bootstrap/Image';
-import { GiAlarmClock } from "react-icons/gi";
-import { RxTimer } from "react-icons/rx";
-import { RxStopwatch } from "react-icons/rx";
-import { LuClock9 } from "react-icons/lu";
-import Stack from 'react-bootstrap/Stack';
-import Button from 'react-bootstrap/Button';
-import { CountdownCircleTimer } from 'react-countdown-circle-timer'
-import styled from 'styled-components';
-
-
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+import styled from "styled-components";
+import React, { useState } from "react";
 
 const Title = styled.h1`
   font-size: 2.5em;
@@ -18,45 +10,53 @@ const Title = styled.h1`
 
 const Wrapper = styled.section`
   padding: 4em;
-  background:rgb(144, 212, 255);
+  background: rgb(144, 212, 255);
 `;
 
+function App() {
+  const [On, setOn] = useState(false);
 
-function App (){
+  function click() {
+    if (On === false) {
+      setOn(true);
+    } else {
+      setOn(false);
+    }
+  }
 
-    
-    return (
-                
-        
-        <div className="Container">
+  const [inputValue, setInputValue] = useState("");
 
-   <Wrapper>
-    <Title>ONLINE TIMER</Title>
-   </Wrapper>
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  function type(e) {
+    setInputValue(e.target.value)
+  }
 
-   <div className="Body">     
-   <Stack gap={1}>
-      <div className="p-2"><button onCl><GiAlarmClock></GiAlarmClock></button></div>
-      <div className="p-2"><button><RxTimer></RxTimer></button></div>
-      <div className="p-2"><button><RxStopwatch></RxStopwatch></button></div>
-      <div className="p-2"><button><LuClock9></LuClock9></button></div>
-    </Stack>
-        
+  
+  return (
+    <div className="Container">
+      <Wrapper>
+        <Title>ONLINE TIMER</Title>
+      </Wrapper>
 
-         <CountdownCircleTimer
-    isPlaying
-    duration={9}
-    colors={['#004777', '#F7B801', '#A30000', '#A30000']}
-    colorsTime={[7, 5, 2, 0]}
-  >
-    {({ remainingTime }) => remainingTime}
-  </CountdownCircleTimer>
-  </div>
+      <div className="Body">
+        <CountdownCircleTimer
+          isPlaying={On}
+          duration={30}
+          colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+          colorsTime={[7, 5, 2, 0]}
+        >
+          {({ remainingTime }) => remainingTime}
+        </CountdownCircleTimer>
+      </div>
 
-  </div>
-
-
-    )
+      <div className="buttons">
+        <button onClick={click}>Play/Pause</button>
+        <input onChange={type}/>
+        <button>Click go</button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
